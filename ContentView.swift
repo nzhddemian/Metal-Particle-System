ec711ea437ca2f80823f1973e0b271918cc9ba4a
nzhddemian <nzhd.demian@gmail.com>
2020-11-11 06:14:20 +0300

    particles_2

---
+++ ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Particle
+//
+//  Created by Demian on 06.11.2020.
+//  Copyright © 2020 Demian. All rights reserved.
+//
+
+import UIKit
+import MetalKit
+class ContentView: UIViewController{
+    var render:Render!
+    var mtlView=MTKView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        mtlView.frame = UIScreen.main.bounds
+        render = Render(mtlView)
+      
+        self.view.addSubview(mtlView)
+    }
+    
+}
---
+++ Particle/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Particle
+//
+//  Created by Demian on 06.11.2020.
+//  Copyright © 2020 Demian. All rights reserved.
+//
+
+import UIKit
+
+let delegate = AppDelegate()
+
+UIApplication.shared.delegate = delegate
+_ = UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(AppDelegate.self))
